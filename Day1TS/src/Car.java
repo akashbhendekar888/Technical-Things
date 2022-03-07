739a59cdd958374dafe82ab28b5b0cbcf4be36d9
Lenovo <Lenovo@DESKTOP-PTQDPUC>
2022-03-07 17:44:57 +0530

    First Day Class of TS

---
+++ Day1TS/src/Car.java
@@ -0,0 +1,24 @@
+
+public class Car {
+
+	void display() {
+		System.out.println("Welcome Students");
+		}
+	void display1() {
+		System.out.println("Welcome M12 Batch Students");
+		}
+	void display2() {
+		System.out.println("Welcome To All Students");
+		}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car obj=new Car();
+		obj.display();
+		obj.display1();
+		obj.display2();
+
+	}
+
+}
---
+++ LeafYear/src/ASCIIvalue.java
@@ -0,0 +1,13 @@
+
+public class ASCIIvalue {
+	public static void main(String args[]) {
+		
+		
+		for(int i=0;i<255;i++) {
+		System.out.println("ASCII value::"+(char)i+" ="+i);
+		
+		}
+		
+	}
+
+}
