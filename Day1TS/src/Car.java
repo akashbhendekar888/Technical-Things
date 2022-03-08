d6f902432d2ccc71a72893a2df235ba4f19e42f7
Lenovo <Lenovo@DESKTOP-PTQDPUC>
2022-03-08 10:16:19 +0530

    Day First Program

--- Day1TS/src/Car.java
+++ Day1TS/src/Car.java
@@ -1,23 +1,30 @@
 
 public class Car {
+	String carName;
+	int carPrize;
+	String carColor;       //variables
+	
 
-	void display() {
-		System.out.println("Welcome Students");
+	void displayCarName() {             //method1
+		carName="Mahindra Thar";
+		System.out.println("Car Name::"+carName);    //print statement
 		}
-	void display1() {
-		System.out.println("Welcome M12 Batch Students");
+	void displayCarPrize() {           //method2
+		carPrize=1500000;
+		System.out.println("Car Prize ::"+carPrize);
 		}
-	void display2() {
-		System.out.println("Welcome To All Students");
+	void displayCarColor() {          //method3
+		carColor="Black";
+		System.out.println("Car color::"+carColor);
 		}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {   //main method
 		// TODO Auto-generated method stub
 		
-		Car obj=new Car();
-		obj.display();
-		obj.display1();
-		obj.display2();
+		Car obj=new Car();                   // initiate the object
+		obj.displayCarName();
+		obj.displayCarPrize();              //invoke the methods
+		obj.displayCarColor();
 
 	}
 
